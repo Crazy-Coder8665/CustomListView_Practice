5eaf8d7447d37ffe94da0fb731da685c8dfb1b14
Muhammad Talha Masood <talha8665i@gmail.com>
2022-08-12 03:52:48 -0700

    List Adapter Completed

--- app/src/main/java/com/example/listviewpractice/listAdapter.java
+++ app/src/main/java/com/example/listviewpractice/listAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,8 +26,19 @@ public class listAdapter extends ArrayAdapter<User> {
         User user = getItem(position);
         if(convertView == null){
 
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listitem,parent,false)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listitem,parent,false);
+
         }
+
+        ImageView imageView = convertView.findViewById(R.id.profileImg);
+        TextView username = convertView.findViewById(R.id.personName);
+        TextView lastMsg = convertView.findViewById(R.id.lastMessage);
+        TextView time = convertView.findViewById(R.id.msgtime);
+
+        imageView.setImageResource(user.imageId);
+        username.setText(user.name);
+        lastMsg.setText(user.lastMessage);
+        time.setText(user.lastMsgTime);
 
         return super.getView(position, convertView, parent);
     }
